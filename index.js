65ca3aab9939128f08013267fb8670d926a67dcf
etamar <etamar.levy@gmail.com>
2021-03-30 18:21:20 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(function(tutorial){
+    const newArr = tutorial.split(" ");
+    return newArr.map(function(word){
+     return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(" ");
+  })
 }
